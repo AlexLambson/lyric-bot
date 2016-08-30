3ee1dc460d4edf345c92a23639a0cde2d12cc61a
Alex Lambson <alexlambson@gmail.com>
2016-08-30 10:45:49 -0600

    I have no clue why these files were in lyricbot...

--- learnc/learnc/Header.h
+++
@@ -1,5 +0,0 @@
-int roll_die(const int sides);
-void sort_int_dsc(int a[], const int size);
-void roll_player(int rolls[], const int ndice);
-int handle_battle(const int nattack, const int ndefend);
-int handle_battle(const int nattack, const int ndefend);
